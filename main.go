9b9ca0e00a5df098c24634850864c55915ec5382
C. J. Anderson <chris@standsure.io>
2022-04-20 20:05:05 -0400

    finish POC

--- main.go
+++ main.go
@@ -5,35 +5,37 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"os"
+	"text/template"
 )
 
+// entry point
 func main() {
 	jsonPath, templatePath := getFlags()
-	outputFlags(jsonPath, templatePath)
-	json, err := readJson(jsonPath)
+	data, err := readJson(jsonPath)
 
 	if err != nil {
 		fmt.Printf("read json error: %s", err)
 		return
 	}
 
-	fmt.Println(json)
+	t := readTemplate(templatePath)
+	t.Execute(os.Stdout, data)
 }
 
-func outputFlags(jsonPath string, templatePath string) {
-	fmt.Printf("json path: %s\n", jsonPath)
-	fmt.Printf("template path: %s\n", templatePath)
-}
-
+// getFlags parses the command-line argumentss
 func getFlags() (jsonPath, templatePath string) {
-	flag.StringVar(&jsonPath, "json-path", "./json.json", "json file path")
-	flag.StringVar(&templatePath, "template-path", "./template.template", "template file path")
+	flag.StringVar(&jsonPath, "json-path", "./sample.json", "json file path")
+	flag.StringVar(&templatePath, "template-path", "./default.template", "template file path")
 	flag.Parse()
 
 	return
 }
 
-func readJson(jsonPath string) (json string, err error) {
+// readJson reads the file at jsonPath
+// and returns a map[string]interface and an error
+// either return value can be nil
+func readJson(jsonPath string) (raw map[string]interface{}, err error) {
 	var data []byte
 	data, err = ioutil.ReadFile(jsonPath)
 
@@ -41,14 +43,13 @@ func readJson(jsonPath string) (json string, err error) {
 		return
 	}
 
-	json = string(data)
-
-	foo := parseJson(data)
-	fmt.Printf("%v", foo)
+	raw = parseJson(data)
 
 	return
 }
 
+// parseJson parses a byte slice to a map with string keys
+// can be nil
 func parseJson(data []byte) (result map[string]interface{}) {
 	var f interface{}
 	err := json.Unmarshal(data, &f)
@@ -58,4 +59,12 @@ func parseJson(data []byte) (result map[string]interface{}) {
 	}
 
 	return result
+}
+
+// readTemplate reads the template found at the path in templatePath
+// and returns the *template.Template
+// IMPORTANT: if the template is invalid, the application will PANIC
+func readTemplate(templatePath string) (t *template.Template) {
+	t = template.Must(template.ParseFiles(templatePath))
+	return
 }
